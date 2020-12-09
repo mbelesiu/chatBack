91d545b097d94ac7cf51ab9b11d7e8ca88dd0d46
mbelesiu <matt.belesiu@gmail.com>
2020-12-09 08:34:35 -0800

    more init

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser')
+
+const path = require('path');
+
+
 const PORT = 3000;
+const app = express();
+
+
+app.use(express.static(path.join(__dirname, '/../public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening on port : ${PORT}`);
+})
